8f8a24fd7ac38169f36b776a38ce28f2a04c4b9d
kevy7 <kvnyang503@gmail.com>
2021-02-11 20:27:11 -0800

    created an input form

---
+++ src/Components/Forms/CreditCardForm/CreditCardForm.js
@@ -0,0 +1,66 @@
+import react from 'react';
+import styles from './CreditCardForm.module.css';
+
+let CreditCardForm = () => {
+
+    let handleSubmit = () => {
+        
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                {/* amount input */}
+                <div>
+                    <label className="label">Amount</label>
+                    <input 
+                        className="input" 
+                        type="text" 
+                        placeholder="" 
+                        name="amount" 
+                        /*value={this.state.eventImage} onChange={this.handleInputChanges}*/ 
+                    />
+                </div>
+                
+                <div>
+                    <label className="label">Credit Card Number</label>
+                    <input 
+                        className="input" 
+                        type="tel" 
+                        placeholder="Card Number" 
+                        pattern="\d*" 
+                        maxlength="16"
+                        name="cardNumber"
+                        /*value={this.state.eventImage} onChange={this.handleInputChanges}*/ 
+                    />
+                </div>
+
+                <label className="label">MM</label>
+                <input 
+                    className="input" 
+                    type="tel" 
+                    pattern="\d*"
+                    maxlength="2"
+                    name="MM"
+                    /*value={this.state.eventImage} onChange={this.handleInputChanges}*/ 
+                />
+                /
+                <label className="label">YYYY</label>
+                <input 
+                    className="input" 
+                    type="text"
+                    pattern="\d*"
+                    maxlength="4"
+                    name="YY"
+                    /*value={this.state.eventImage} onChange={this.handleInputChanges}*/ 
+                />
+                
+                <div>
+                    <input type="submit" value="Submit" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default CreditCardForm;
